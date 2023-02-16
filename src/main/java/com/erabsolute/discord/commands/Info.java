fff51796c789ae8ab8b7c20957f0040d50eb9ec3
erAbsolute <a.perezrubio00@gmail.com>
2023-02-16 23:20:16 +0100

    Add qrfy command

--- src/main/java/com/erabsolute/discord/commands/Info.java
+++ src/main/java/com/erabsolute/discord/commands/Info.java
@@ -1,6 +1,21 @@
 package com.erabsolute.discord.commands;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
@@ -20,8 +35,8 @@ public class Info {
 	}
 
 	public EmbedBuilder ping(JDA jda, User user, Guild guild) {
-		long ping = jda.getGatewayPing();
 		EmbedBuilder embed = _getDefaultEmbed(user, guild);
+		long ping = jda.getGatewayPing();
 		if (ping <= 30) {
 			embed.setColor(Color.green);
 		} else if (ping >= 31 && ping <= 150) {
@@ -30,6 +45,41 @@ public class Info {
 			embed.setColor(Color.red);
 		}
 		embed.setDescription("La latencia es de " + ping + " ms.");
+		return embed;
+	}
+
+	public EmbedBuilder qrfy(User user, Guild guild) {
+		EmbedBuilder embed = _getDefaultEmbed(user, guild);
+		embed.setImage("attachment://qr.png");
+		embed.setThumbnail(null);
+		return embed;
+	}
+
+	public ByteArrayOutputStream generateQRCodeImage(String barcodeText) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		QRCodeWriter barcodeWriter = new QRCodeWriter();
+		File logoFile = new File("resources/logo.png");
+		Map<EncodeHintType, ErrorCorrectionLevel> hints = new HashMap<>();
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+		BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 4096, 4096, hints);
+		BufferedImage logo = ImageIO.read(logoFile);
+		BufferedImage qr = MatrixToImageWriter.toBufferedImage(bitMatrix);
+		BufferedImage combined = new BufferedImage(4096, 4096, BufferedImage.TYPE_INT_ARGB);
+		// paint both images, preserving the alpha channels
+		Graphics g = combined.getGraphics();
+		g.drawImage(qr, 0, 0, null);
+		g.drawImage(logo, 1488, 1488, null);
+		g.fillRect(20, 20, 100, 100);
+		g.dispose();
+
+		ImageIO.write(combined, "png", baos);
+		return baos;
+	}
+
+	public EmbedBuilder errorEmbed(User user, Guild guild) {
+		EmbedBuilder embed = _getDefaultEmbed(user, guild);
+		embed.setColor(Color.red);
+		embed.setDescription("El comando o los parámetros introducidos, no son válidos.");
 		return embed;
 	}
 
--- src/main/java/com/erabsolute/discord/listeners/CommandsListener.java
+++ src/main/java/com/erabsolute/discord/listeners/CommandsListener.java
@@ -1,5 +1,6 @@
 package com.erabsolute.discord.listeners;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +15,10 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.utils.FileUpload;
 
 public class CommandsListener extends ListenerAdapter {
 	Logger logs = LoggerFactory.getLogger(CommandsListener.class);
@@ -28,20 +31,36 @@ public class CommandsListener extends ListenerAdapter {
 		event.deferReply().setEphemeral(true).queue();
 		switch (command) {
 		case "help":
-			EmbedBuilder embed = info.help(user, guild);
-			event.getHook().sendMessageEmbeds(embed.build()).queue();
+			EmbedBuilder helpEmbed = info.help(user, guild);
+			event.getHook().sendMessageEmbeds(helpEmbed.build()).queue();
 			break;
 		case "ping":
 			EmbedBuilder pingEmbed = info.ping(event.getJDA(), user, guild);
 			event.getHook().sendMessageEmbeds(pingEmbed.build()).setEphemeral(true).queue();
+			break;
+		case "qrfy":
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			;
+			try {
+				baos = info.generateQRCodeImage(event.getOptions().get(0).getAsString());
+			} catch (Exception e) {
+				logs.error(e.getMessage());
+				event.getHook().sendMessageEmbeds(info.errorEmbed(user, guild).build()).setEphemeral(true).queue();
+				break;
+			}
+			EmbedBuilder qrfyEmbed = info.qrfy(user, guild);
+			event.getHook().sendMessageEmbeds(qrfyEmbed.build()).setEphemeral(true)
+					.addFiles(FileUpload.fromData(baos.toByteArray(), "qr.png")).queue();
 			break;
 		}
 	}
 
 	public void onGuildReady(GuildReadyEvent event) {
 		List<CommandData> commandData = new ArrayList<>();
-		commandData.add(Commands.slash("help", "Muestra los comandos disponibles"));
-		commandData.add(Commands.slash("ping", "Devuelve la latencia del bot"));
+		commandData.add(Commands.slash("help", "Muestra los comandos disponibles."));
+		commandData.add(Commands.slash("ping", "Devuelve la latencia del bot."));
+		commandData.add(Commands.slash("qrfy", "Genera un código QR con el texto que le pases.")
+				.addOption(OptionType.STRING, "input", "Texto que queremos pasar a QR"));
 		event.getGuild().updateCommands().addCommands(commandData).queue();
 	}
 }
