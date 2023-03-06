74bdee8a40c6c5232e236745ef2ed2553db22b3e
erAbsolute <a.perezrubio00@gmail.com>
2023-03-06 16:43:51 +0100

    Add RegEx functions

--- src/main/java/com/erabsolute/discord/listeners/CommandsListener.java
+++ src/main/java/com/erabsolute/discord/listeners/CommandsListener.java
@@ -3,6 +3,8 @@ package com.erabsolute.discord.listeners;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,7 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -21,35 +24,38 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.utils.FileUpload;
 
 public class CommandsListener extends ListenerAdapter {
-	Logger logs = LoggerFactory.getLogger(CommandsListener.class);
+
+	private Logger logs = LoggerFactory.getLogger(CommandsListener.class);
+
+	private Info info = new Info();
 
 	public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-		Info info = new Info();
 		String command = event.getName();
 		User user = event.getUser();
 		Guild guild = event.getGuild();
 		event.deferReply().setEphemeral(true).queue();
 		switch (command) {
 		case "help":
+			logs.info(user.getName() + user.getDiscriminator() + " requested the command " + command);
 			EmbedBuilder helpEmbed = info.help(user, guild);
 			event.getHook().sendMessageEmbeds(helpEmbed.build()).queue();
 			break;
 		case "ping":
+			logs.info(user.getName() + user.getDiscriminator() + " requested the command " + command);
 			EmbedBuilder pingEmbed = info.ping(event.getJDA(), user, guild);
 			event.getHook().sendMessageEmbeds(pingEmbed.build()).setEphemeral(true).queue();
 			break;
 		case "qrfy":
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			try {
-				baos = info.generateQRCodeImage(event.getOptions().get(0).getAsString());
+			logs.info(user.getName() + user.getDiscriminator() + " requested the command " + command);
+			try (ByteArrayOutputStream baos = info.generateQRCodeImage(event.getOptions().get(0).getAsString());) {
+				EmbedBuilder qrfyEmbed = info.qrfy(user, guild);
+				event.getHook().sendMessageEmbeds(qrfyEmbed.build()).setEphemeral(true)
+						.addFiles(FileUpload.fromData(baos.toByteArray(), "qr.png")).queue();
+				break;
 			} catch (Exception e) {
 				event.getHook().sendMessageEmbeds(info.errorEmbed(user, guild).build()).setEphemeral(true).queue();
 				break;
 			}
-			EmbedBuilder qrfyEmbed = info.qrfy(user, guild);
-			event.getHook().sendMessageEmbeds(qrfyEmbed.build()).setEphemeral(true)
-					.addFiles(FileUpload.fromData(baos.toByteArray(), "qr.png")).queue();
-			break;
 		}
 	}
 
@@ -60,5 +66,26 @@ public class CommandsListener extends ListenerAdapter {
 		commandData.add(Commands.slash("qrfy", "Genera un código QR con el texto que le pases.")
 				.addOption(OptionType.STRING, "input", "Texto que queremos pasar a QR"));
 		event.getGuild().updateCommands().addCommands(commandData).queue();
+	}
+
+	private static final Pattern quesoRegex = Pattern.compile("(?i).*(que|qe|q)\\b");
+	private static final Pattern discordInvitePattern = Pattern.compile(
+			"(https?:\\/\\/)?(www\\.)?(discord\\.(gg|io|me|li)|discordapp\\.com\\/invite)\\/[a-zA-Z0-9]+",
+			Pattern.CASE_INSENSITIVE);
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		String message = event.getMessage().getContentRaw();
+		Matcher queso = quesoRegex.matcher(message);
+		Matcher reportInvite = discordInvitePattern.matcher(message);
+		if (queso.matches()) {
+			event.getMessage().reply("so");
+		} else if (reportInvite.matches()) {
+			EmbedBuilder report = info.errorEmbed(event.getAuthor(), event.getGuild());
+			report.setDescription("El usuario " + event.getAuthor().getName() + event.getAuthor().getDiscriminator()
+					+ " ha posteado una invitación.");
+			event.getChannel().sendMessageEmbeds(report.build()).queue();
+		}
+
 	}
 }
